bf170621689e326ef316051ea832cac5064110bc
Jagannath A <jagannath.sysop@gmail.com>
2019-03-17 16:44:02 +0530

    adds remaining layout, styles and themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Simple Interest Calculator',
-    home: SIForm(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Interest Calculator',
+      home: SIForm(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent,
+      ),
+    ),
+  );
 }
 
 class SIForm extends StatefulWidget {
@@ -13,18 +21,22 @@ class SIForm extends StatefulWidget {
 }
 
 class _SIFormState extends State<SIForm> {
-  var _currencies = ['Rupees ₹', 'Dollars \$', 'Pounds £'];
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
   final _minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
+
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
     return Scaffold(
+//      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
       ),
       body: Container(
         margin: EdgeInsets.all(_minimumPadding * 2),
-        child: Column(
+        child: ListView(
           children: <Widget>[
             getImageAsset(),
             Padding(
@@ -32,9 +44,11 @@ class _SIFormState extends State<SIForm> {
                   top: _minimumPadding, bottom: _minimumPadding),
               child: TextField(
                 keyboardType: TextInputType.number,
+                style: textStyle,
                 decoration: InputDecoration(
                     labelText: 'Principal',
                     hintText: 'Enter Principal',
+                    labelStyle: textStyle,
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(5.0))),
               ),
@@ -44,12 +58,82 @@ class _SIFormState extends State<SIForm> {
                   top: _minimumPadding, bottom: _minimumPadding),
               child: TextField(
                 keyboardType: TextInputType.number,
+                style: textStyle,
                 decoration: InputDecoration(
                     labelText: 'Rate of Interest',
                     hintText: 'In percent',
+                    labelStyle: textStyle,
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(5.0))),
               ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      decoration: InputDecoration(
+                          labelText: 'Term',
+                          hintText: 'Time in years',
+                          labelStyle: textStyle,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: 'Rupees',
+                      onChanged: (String newValueSelected) {
+                        // code to execute after a menu item is selected
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).accentColor,
+                      textColor: Theme.of(context).primaryColorDark,
+                      child: Text('Calculate', textScaleFactor: 1.5,),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Theme.of(context).primaryColorDark,
+                      textColor: Theme.of(context).primaryColorLight,
+                      child: Text('Reset', textScaleFactor: 1.5,),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(_minimumPadding * 2),
+              child: Text('Result Text', style: textStyle,),
             ),
           ],
         ),
@@ -61,13 +145,13 @@ class _SIFormState extends State<SIForm> {
     AssetImage assetImage = AssetImage('assets/images/inr-symbol.png');
     Image image = Image(
       image: assetImage,
-      width: 125.0,
-      height: 125.0,
+      width: 100.0,
+      height: 100.0,
     );
 
     return Container(
       child: image,
-      margin: EdgeInsets.all(_minimumPadding * 10),
+      margin: EdgeInsets.all(_minimumPadding * 5),
     );
   }
 }
